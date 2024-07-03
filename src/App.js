7b4bf541488703731077ccc608bc63aafca44d08
weiwei <wutamy77@gmail.com>
2024-07-03 19:48:03 +0800

    create page and rwd layout

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ function App() {
     <div className="App">
       <div className="page" style={{ display: "flex" }}>
         <div className={`menu ${isHamMenuOpen ? 'open' : ''}`}>
-        <div class={`hamburgerMenu ${isHamMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-              <div class="bar"></div>
-              <div class="bar"></div>
-              <div class="bar"></div>
+        <div className={`hamburgerMenu ${isHamMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
           </div>
           <div className="birdContainer">
               <div className="bird">
@@ -66,7 +66,7 @@ function App() {
             <div className="introbox">
               <div className="title">
                 <b>外觀</b>
-                <div class="donut"></div>
+                <div className="donut"></div>
               </div>
               <span className="text">
                 白頭鵯體長約17到22公分，額至頭頂純黑色而富有光澤，兩眼上方至後枕白色，形成一白色枕環。耳羽後部有一白斑，此白環與白斑在黑色的頭部均極為醒目，老鳥的枕羽(後頭部)更潔白，所以又叫「白頭翁」。
@@ -75,7 +75,7 @@ function App() {
             <div className="introbox">
               <span className="title">
                 <b>棲地</b>
-                <div class="donut"></div>
+                <div className="donut"></div>
               </span>
               <span className="text">
                 白頭翁和麻雀、綠繡眼合稱「城市三寶」，常成群出現在平原區灌木叢、丘陵樹林地帶，以及校園、公園、庭院、行道中的各種高高的電線與樹上。
@@ -84,7 +84,7 @@ function App() {
             <div className="introbox">
               <span className="title">
                 <b>食性</b>
-                <div class="donut"></div>
+                <div className="donut"></div>
               </span>
               <span className="text">
                 以果樹的漿果和種子為主食，並時常飛入果園偷吃果實，還會吃嫩葉嫩芽，尤其是胡蝶蘭的嫩葉嫩芽葉，偶爾啄食昆蟲。
